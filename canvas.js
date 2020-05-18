5fbbefab0130ad24d039b4b1d9f5f635d759c701
Keith <42125082+kbarbag@users.noreply.github.com>
2020-05-18 11:54:34 -0700

    further abstract away main object

--- canvas.js
+++ canvas.js
@@ -1,23 +1,9 @@
 import { Arc, Bar } from './models/models.js';
 
-let userData = [{ name: "Fanny Nelson", age: 22, height: 4 },
-{ name: "Bill Foster", age: 25, height: 7 },
-{ name: "Willie Underwood", age: 23, height: 4 },
-{ name: "Danny Brady", age: 24, height: 6 },
-{ name: "Ricky Williamson", age: 24, height: 4 },
-{ name: "Beulah Brock", age: 23, height: 7 },
-{ name: "Cameron Salazar", age: 23, height: 5 },
-{ name: "Corey Nguyen", age: 20, height: 3 },
-{ name: "Mabel Murray", age: 22, height: 5 },
-{ name: "Elva Garrett", age: 21, height: 5 }];
-
-const graphId = 'graph';
-
-let canvas = document.getElementById(graphId);
+let canvas = {};
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-let graph = canvas.getContext('2d');
-let rect = canvas.getBoundingClientRect();
+let rect = { left: 0, top: 0 };
 
 window.addEventListener('mousemove', function (event) {
     let scaleX = canvas.width / rect.width,
@@ -34,78 +20,89 @@ window.addEventListener('click', function (event) {
     console.log(`click: x(${x}), y(${y})`);
 });
 
-function cycleHexColor(input) {
-    input += 1;
-    if (input === 7) input = 0;
-    let bin = input.toString(2);
-    bin = '000'.substring(bin.length) + bin;
-    let returnHexColor = '#';
-    for (let i = 0; i < bin.length; i++) {
-        if (bin.charAt(i) === '1') returnHexColor += 'F';
-        else returnHexColor += '0';
+
+
+let DataChart = function DataChart(data, graphId) {
+    this.data = data;
+    this.graphId = graphId;
+    this.canvas = document.getElementById(this.graphId);
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+    this.graph = this.canvas.getContext('2d');
+    this.rect = this.canvas.getBoundingClientRect();
+    canvas = this.canvas;
+    rect = this.rect;
+
+    this.cycleHexColor = function (input) {
+        input += 1;
+        if (input === 7) input = 0;
+        let bin = input.toString(2);
+        bin = '000'.substring(bin.length) + bin;
+        let returnHexColor = '#';
+        for (let i = 0; i < bin.length; i++) {
+            if (bin.charAt(i) === '1') returnHexColor += 'F';
+            else returnHexColor += '0';
+        }
+        return { hex: returnHexColor, decimal: input };
     }
-    return { hex: returnHexColor, decimal: input };
+
+    this.draw = function () {
+        this.graph.clearRect(0, 0, window.innerWidth, window.innerHeight);
+        this.graph.font = '1.4em sans-serif';
+        let groupBy = 'height';
+        let maxVal, minVal;
+        for (let i = 0; i < this.data.length; i++) {
+            maxVal = maxVal ? Math.max(maxVal, this.data[i][groupBy]) : this.data[i][groupBy];
+            minVal = minVal ? Math.min(minVal, this.data[i][groupBy]) : this.data[i][groupBy];
+        }
+        let spacer = ((this.data.length + 10) / this.data.length) * 10;
+        let barWidth = (this.canvas.width / this.data.length) - (spacer / 1);
+        let chartHeight = maxVal * 1.25;
+
+        let startColor = 0;
+        let space = spacer;
+        let colors;
+        //draw bar graph
+        for (let i = 0; i < this.data.length; i++) {
+            let x = Math.floor(i * (barWidth + space / 2) + ((i + 1) * (space / 2)));
+            let user = this.data[i];
+            let compareData = user[groupBy];
+            let height = Math.floor((compareData / chartHeight) * this.canvas.height);
+            colors = this.cycleHexColor(startColor);
+            startColor = colors.decimal;
+            let fillColor = colors.hex;
+            let bar = new Bar(x, this.canvas.height - height, barWidth, height, spacer, `${fillColor}`, `${fillColor}`, user, compareData, this.graphId);
+            bar.draw();
+        }
+
+        let groupings = {};
+        for (let i = 0; i < this.data.length; i++) {
+            let user = this.data[i];
+            let group = user[groupBy];
+            if (!(group in groupings)) groupings[group] = [];
+            groupings[group].push(user.name);
+        }
+        let x = this.canvas.width / 2, y = this.canvas.height / 2;
+        let radius = (this.canvas.height / 2) * 0.8;
+        let startAngle = 0, endAngle = 0;
+
+        let lastPercent = 0;
+        let fillColor = '';
+        startColor = 0;
+        //draw pie chart
+        for (let [key, value] of Object.entries(groupings)) {
+            colors = this.cycleHexColor(startColor);
+            startColor = colors.decimal;
+            fillColor = colors.hex;
+            let groupCount = value.length;
+            let groupPercent = groupCount / this.data.length;
+            lastPercent += groupPercent;
+            startAngle = endAngle;
+            endAngle = 2 * Math.PI * (1 - (lastPercent));
+            let arc = new Arc(x, y, radius, startAngle, endAngle, fillColor, fillColor, `${key}`, this.graphId);
+            arc.draw();
+        }
+    }
 }
 
-function draw() {
-    graph.clearRect(0, 0, window.innerWidth, window.innerHeight);
-    graph.font = '1.4em sans-serif';
-    let groupBy = 'height';
-    let maxVal, minVal;
-    for (let i = 0; i < userData.length; i++) {
-        maxVal = maxVal ? Math.max(maxVal, userData[i][groupBy]) : userData[i][groupBy];
-        minVal = minVal ? Math.min(minVal, userData[i][groupBy]) : userData[i][groupBy];
-    }
-    let spacer = ((userData.length + 10) / userData.length) * 10;
-    let barWidth = (canvas.width / userData.length) - (spacer / 1);
-    let chartHeight = maxVal * 1.25;
-
-    let startColor = 0;
-    let space = spacer;
-    let colors;
-    //draw bar graph
-    for (let i = 0; i < userData.length; i++) {
-        let x = Math.floor(i * (barWidth + space / 2) + ((i + 1) * (space / 2)));
-        let user = userData[i];
-        let compareData = user[groupBy];
-        let height = Math.floor((compareData / chartHeight) * canvas.height);
-        colors = cycleHexColor(startColor);
-        startColor = colors.decimal;
-        let fillColor = colors.hex;
-        let bar = new Bar(x, canvas.height - height, barWidth, height, spacer, `${fillColor}`, `${fillColor}`, user, compareData, graphId);
-        bar.draw();
-    }
-
-    let groupings = {};
-    for (let i = 0; i < userData.length; i++) {
-        let user = userData[i];
-        let group = user[groupBy];
-        if (!(group in groupings)) groupings[group] = [];
-        groupings[group].push(user.name);
-    }
-    let x = canvas.width / 2, y = canvas.height / 2;
-    let radius = (canvas.height / 2) * 0.8;
-    let startAngle = 0, endAngle = 0;
-
-    let lastPercent = 0;
-    let fillColor = '';
-    startColor = 0;
-    //draw pie chart
-    for (let [key, value] of Object.entries(groupings)) {
-        colors = cycleHexColor(startColor);
-        startColor = colors.decimal;
-        fillColor = colors.hex;
-        let groupCount = value.length;
-        let groupPercent = groupCount / userData.length;
-        lastPercent += groupPercent;
-        startAngle = endAngle;
-        endAngle = 2 * Math.PI * (1 - (lastPercent));
-        let arc = new Arc(x, y, radius, startAngle, endAngle, fillColor, fillColor, `${key}`, graphId);
-        arc.draw();
-    }
-    window.requestAnimationFrame(draw);
-}
-
-
-
-window.requestAnimationFrame(draw);
+export default DataChart;
