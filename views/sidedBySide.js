72b9171e9c50bbc5d4d724fb1d42fc8f39ca4d5e
Keith <42125082+kbarbag@users.noreply.github.com>
2020-06-06 03:26:07 -0700

    modularize view header component

--- views/sidedBySide.js
+++ views/sidedBySide.js
@@ -1,3 +1,4 @@
+import Header from './header.js';
 const { Elements } = require('../enums/enums.js');
 
 const SideBySide = function ({ graphWrapperId, categories, selectedCategory }) {
@@ -9,88 +10,12 @@ const SideBySide = function ({ graphWrapperId, categories, selectedCategory }) {
 }
 
 SideBySide.prototype.create = function () {
-    //create first row (header)
-    let insertElement = this.graphWrapper;
-    let element = document.createElement('div');
-    element.classList = 'row'
-    element.id = `${this.graphWrapperId}${Elements.header}`;
-    insertElement.appendChild(element);
-    insertElement = element;
-
-    //category label
-    element = document.createElement('div');
-    element.classList = 'col-sm-1';
-    element.innerHTML = 'Category Group:';
-    insertElement.appendChild(element);
-
-    //category wrapper
-    element = document.createElement('div');
-    element.id = `${this.graphWrapperId}${Elements.categoryWrapper}`;
-    element.classList = 'col-sm-3';
-    insertElement.appendChild(element);
-    insertElement = element;
-
-    //category select
-    element = document.createElement('select');
-    element.id = `${this.graphWrapperId}${Elements.categorySelect}`;
-    element.classList = 'form-control';
-
-    this.categories.forEach(category => {
-        element.add(new Option(`${category}`, `${category}`));
-    });
-    element.value = this.selectedCategory.val;
-    insertElement.appendChild(element);
-
-    //graph type label
-    insertElement = document.getElementById(`${this.graphWrapperId}${Elements.header}`);
-    element = document.createElement('div');
-    element.classList = 'col-sm-1';
-    element.innerHTML = 'Graph Type:';
-    insertElement.appendChild(element);
-
-    //graph type wrapper
-    element = document.createElement('div');
-    element.id = `${this.graphWrapperId}${Elements.graphTypeWrapper}`;
-    element.classList = 'col-sm-2';
-    insertElement.appendChild(element);
-    insertElement = element;
-
-    //graph type select
-    element = document.createElement('select');
-    element.id = `${this.graphWrapperId}${Elements.graphType}`;
-    element.classList = 'form-control';
-    element.add(new Option('Pie Chart', '0'));
-    element.add(new Option('Bar Graph', '1'));
-    insertElement.appendChild(element);
-
-    //increments label
-    insertElement = document.getElementById(`${this.graphWrapperId}${Elements.header}`);
-    element = document.createElement('div');
-    element.id = `${this.graphWrapperId}${Elements.incrementsLbl}`
-    element.classList = 'col-sm-1';
-    element.innerHTML = 'Increments:'
-    insertElement.appendChild(element);
-
-    //increments wrapper
-    element = document.createElement('div');
-    element.id = `${this.graphWrapperId}${Elements.incrementsWrpr}`
-    element.classList = 'col-sm-1';
-    insertElement.appendChild(element);
-    insertElement = element;
-
-    //increments field
-    element = document.createElement('input');
-    element.setAttribute('type', 'number');
-    element.setAttribute('min', '1');
-    element.setAttribute('value', 1);
-    element.classList = 'form-control'
-    element.id = `${this.graphWrapperId}${Elements.increments}`;
-    insertElement.appendChild(element);
+    new Header({ graphWrapperId: this.graphWrapperId, categories: this.categories, selectedCategory: this.selectedCategory });
 
 
     //create 2nd row
-    insertElement = this.graphWrapper;
-    element = document.createElement('div');
+    let insertElement = this.graphWrapper;
+    let element = document.createElement('div');
     element.classList = 'row';
     insertElement.appendChild(element);
     insertElement = element;
